1da703a2754611262006f5b2c74485cc2cab5682
hlaingwintun <sv.hlaingwt@gmail.com>
2017-11-26 06:31:15 +0800

    active profile set programatically

--- src/main/java/com/concretepage/MyApplication.java
+++ src/main/java/com/concretepage/MyApplication.java
@@ -3,11 +3,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 @SpringBootApplication
 public class MyApplication {
 	private static final Logger logger = LoggerFactory.getLogger(MyApplication.class);	
 	public static void main(String[] args) {
-		SpringApplication.run(MyApplication.class, args);
+		SpringApplication application = new SpringApplication(MyApplication.class);
+		ConfigurableEnvironment env = new StandardEnvironment();
+		env.setDefaultProfiles("dev" , "animal_dev");
+		application.setEnvironment(env);
+		application.run(args);
 		logger.debug("--Application Started--");		
     }       
 }            
